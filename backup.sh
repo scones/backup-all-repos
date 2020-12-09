56c9f6399c8b4cebc5724af7357ec4267d036578
Dirk Gustke <dirk.gustke@verticalmedia.com>
2020-12-09 14:27:12 +0100

    finished scripts

--- backup.sh
+++ backup.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+organisation="vmpublishing/"
+
+rm -rf repo-backup/*
+while IFS="" read -r repo || [ -n "$repo" ]
+do
+  project=${repo#"$organisation"}
+  printf 'backup %s\n' "$project"
+  $(cd repo-backup; git clone --quiet "git@github.com:${repo}.git" "$project" > /dev/null)
+done < repos_names.txt
+tar czvf repo-backup.tar.gz repo-backup
 
